6697fb8ec9913c7530521e1ed7c745ef50df072d
arin zbook <arin_muk@hotmail.com>
2022-01-05 19:16:46 -0500

    year 2022 added to plots

--- static/js/plot3.js
+++ static/js/plot3.js
@@ -1,4 +1,5 @@
 //console.log(data1);
+var ayr20222 = {}
 var ayr20212 = {}
 var ayr20202 = {}
 var ayr20192 = {}
@@ -13,6 +14,10 @@ var ayr20112 = {}
 var data=[]
 var data1=[]
 
+
+function ayear20222(sale) {
+        return sale.year == '2022';
+      }
 function ayear20212(sale) {
         return sale.year == '2021';
       }
@@ -59,28 +64,34 @@ var loc =0
 function ainittwo(){
 
         //console.log(yr2019)
-        var trace={x:ayr20212.map(row=>row.month),
+        var trace={x:ayr20222.map(row=>row.month),
+                y:ayr20222.map(row=>row.NetRecd),
+                name:"2022",
+                mode: 'markers',
+                 type:"scatter",
+                 marker: { size: ayr20222.map(row=>row.NetRecd/100) }}  
+        var trace1={x:ayr20212.map(row=>row.month),
                 y:ayr20212.map(row=>row.NetRecd),
                 name:"2021",
                 mode: 'markers',
                  type:"scatter",
                  marker: { size: ayr20212.map(row=>row.NetRecd/100) }}
 
-        var trace1={x:ayr20202.map(row=>row.month),
+        var trace2={x:ayr20202.map(row=>row.month),
                 y:ayr20202.map(row=>row.NetRecd),
                 name:"2020",
                 mode: 'markers',
                  type:"scatter",
                  marker: { size: ayr20202.map(row=>row.NetRecd/100) }
       }
-        var trace2={x:ayr20192.map(row=>row.month),
+        var trace3={x:ayr20192.map(row=>row.month),
                    y:ayr20192.map(row=>row.NetRecd),
                    name:"2019",
                    mode: 'markers',
                     type:"scatter",
                     marker: { size: ayr20192.map(row=>row.NetRecd/100) }
          }
-        var trace3={x:ayr20182.map(row=>row.month),
+        var trace4={x:ayr20182.map(row=>row.month),
                 y:ayr20182.map(row=>row.NetRecd),
                 name:"2018",
                 mode: 'markers',
@@ -88,7 +99,7 @@ function ainittwo(){
                  marker: { size: ayr20182.map(row=>row.NetRecd/100) }
         }
         
-         var trace4={x:ayr20172.map(row=>row.month),
+         var trace5={x:ayr20172.map(row=>row.month),
                 y:ayr20172.map(row=>row.NetRecd),
                 name:"2017",
                 mode: 'markers',
@@ -96,7 +107,7 @@ function ainittwo(){
                  marker: { size: ayr20172.map(row=>row.NetRecd/100) }
                 
          }
-         var trace5={x:ayr20162.map(row=>row.month),
+         var trace6={x:ayr20162.map(row=>row.month),
                 y:ayr20162.map(row=>row.NetRecd),
                 name:"2016",
                 mode: 'markers',
@@ -104,42 +115,42 @@ function ainittwo(){
                  marker: { size: ayr20162.map(row=>row.NetRecd/100) }
         }
 
-         var trace6={x: ayr20152.map(row=>row.month),
+         var trace7={x: ayr20152.map(row=>row.month),
                      y: ayr20152.map(row=>row.NetRecd),
                         name:"2015",
                         mode: 'markers',
                          type:"scatter",
                          marker: { size: ayr20152.map(row=>row.NetRecd/100) }
         }
-        var trace7={x: ayr20142.map(row=>row.month),
+        var trace8={x: ayr20142.map(row=>row.month),
                 y: ayr20142.map(row=>row.NetRecd),
                    name:"2014",
                    mode: 'markers',
                     type:"scatter",
                     marker: { size: ayr20142.map(row=>row.NetRecd/100) }
    }
-        var trace8={x: ayr20132.map(row=>row.month),
+        var trace9={x: ayr20132.map(row=>row.month),
                 y: ayr20132.map(row=>row.NetRecd),
                 name:"2013",
                 mode: 'markers',
                 type:"scatter",
                 marker: { size: ayr20132.map(row=>row.NetRecd/100) }
 }    
-        var trace9={x: ayr20122.map(row=>row.month),
+        var trace10={x: ayr20122.map(row=>row.month),
         y: ayr20122.map(row=>row.NetRecd),
         name:"2012",
         mode: 'markers',
         type:"scatter",
         marker: { size: ayr20122.map(row=>row.NetRecd/100) }
 } 
-        var trace10={x: ayr20112.map(row=>row.month),
+        var trace11={x: ayr20112.map(row=>row.month),
         y: ayr20112.map(row=>row.NetRecd),
         name:"2011",
         mode: 'markers',
         type:"scatter",
         marker: { size: ayr20112.map(row=>row.NetRecd/100) }
 }            
-        var data=[trace,trace1,trace2,trace3,trace4,trace5,trace6,trace7,trace8,trace9,trace10]
+        var data=[trace,trace1,trace2,trace3,trace4,trace5,trace6,trace7,trace8,trace9,trace10,trace11]
 
          var layout ={
                 title:"Monthly Sales Net Received",
@@ -171,60 +182,66 @@ function getData3(dataset){
         case "All":
          init()
          break
+         case "2022":
+                x=ayr20222.map(row=>row.month),
+                y=ayr20222.map(row=>row.NetRecd)
+                loc=0
+                break
+
          case "2021":
                 x=ayr20212.map(row=>row.month),
                 y=ayr20212.map(row=>row.NetRecd)
-                loc=0
+                loc=1
                 break
          case "2020":
          x=ayr20202.map(row=>row.month),
          y=ayr20202.map(row=>row.NetRecd)
-         loc=1
+         loc=2
          break;
          case "2019":
          x=ayr20192.map(row=>row.month),
          y=ayr20192.map(row=>row.NetRecd)
-         loc=2
+         loc=3
          break;
         case "2018":
          x=ayr20182.map(row=>row.month),
          y=ayr20182.map(row=>row.NetRecd)
-         loc=3
+         loc=4
                         break;
         case "2017":
           x=ayr20172.map(row=>row.month),
           y=ayr20172.map(row=>row.NetRecd)
-          loc=4
+          loc=5
                         break;
         case "2016":
            x=ayr20162.map(row=>row.month),
            y=ayr20162.map(row=>row.NetRecd)
-           loc=5
+           loc=6
                          break; 
         case "2015":
          x=ayr20152.map(row=>row.month),
          y=ayr20152.map(row=>row.NetRecd)
-         loc=6
+         loc=7
                 break;                                                                                                                     
         case "2014":
           x=ayr20142.map(row=>row.month),
           y=ayr20142.map(row=>row.NetRecd)
-         loc=7
+         loc=8
         break;
         case "2013":
          x=ayr20132.map(row=>row.month),
          y=ayr20132.map(row=>row.NetRecd)
-         loc=8
+         loc=9
          break;              
          case "2012":
          x=ayr20122.map(row=>row.month),
          y=ayr20122.map(row=>row.NetRecd)
-         loc=9
+         loc=10
          break; 
          case "2011":
                         x=ayr20112.map(row=>row.month),
                         y=ayr20112.map(row=>row.NetRecd)
-                        loc=10
+                        loc=11
                         break;                
 
         default:
@@ -243,7 +260,7 @@ function getData3(dataset){
 function aupdatePlotlytwo(newx,newy,loc){
         ainittwo()
         arrtrc=[]
-        origarrtrc=[0,1,2,3,4,5,6,7,8,9,10]
+        origarrtrc=[0,1,2,3,4,5,6,7,8,9,10,11]
         for(i=0;i<origarrtrc.length;i++){
                 if(origarrtrc[i] != loc){arrtrc.push(origarrtrc[i])}
         }
@@ -255,11 +272,12 @@ function aupdatePlotlytwo(newx,newy,loc){
         Plotly.restyle(LINE,"y",[newy])
 }
 // YOUR CODE HERE
-urlstring='https://aircraft-apis.herokuapp.com/readSales'
+urlstring='http://127.0.0.1:5000/readSales'
 
 d3.json(urlstring).then(function(sample_m) {
         data1=sample_m
         data=data1
+ayr20222 = data1.filter(ayear20212)
 ayr20212 = data1.filter(ayear20212)
 ayr20202 = data1.filter(ayear20202)
 ayr20192 = data1.filter(ayear20192)
